f2cc0606a4fac2b238cdafffa8b56b55551f26aa
DynamicRemo <dynamicremo@gmail.com>
2017-02-22 14:00:29 +0100

    API Updated

--- routes/mongoapi.js
+++ routes/mongoapi.js
@@ -5,9 +5,6 @@ var async = require("async");
 var currentDB;
 
 router.get('/all_collections', function(req, res, next) {
-	// console.log("CurrentDB");
-	// console.log(currentDB);
-
 	currentDB.listCollections().toArray(function(err, collInfos) {
 		console.log("All Collections");
 		console.log("Number of Collections: " + collInfos.length );
@@ -26,13 +23,11 @@ router.get('/all_collections_count', function(req, res, next) {
 	    if (!err) {
 	    	var findCounts = function(coll,callback){
 				currentDB.collection(coll.name).count({}, function(error, numOfDocs){
-		        	// console.log(coll.name + " - " + numOfDocs);
 		            callback(null, numOfDocs);
 		        });
 		    }
 
 			async.map(collInfos, findCounts, function(err, results){
-				// console.log("ASYNCCCC");
 				console.dir(results);
 			    if (!err) {
 			    	res.send(results);
@@ -47,23 +42,15 @@ router.get('/all_collections_count', function(req, res, next) {
 });
 
 var MongoClient = require('mongodb').MongoClient;
-// var assert = require('assert');
 
 // Connection URL 
-var url = 'mongodb://localhost:27017/mongodbapi';
+// var url = 'mongodb://localhost:27017/mongodbapi';
+var url = 'mongodb://<connection-url>/<database-name>';
 // Use connect method to connect to the Server 
 MongoClient.connect(url, function(err, db) {
 	// For the Global Variable
 	currentDB = db;
-	// assert.equal(null, err);
-	console.log("Connected correctly to server");
-	// insertDocuments(db, function() {
-	// 	findCollections(db, function() {
-	// 		findDocuments(db, function() {
-	// 			db.close();
-	// 		});
-	// 	});
-	// });
+	console.log("Server Connected");
 });
 
 var insertDocuments = function(db, callback) {
@@ -73,9 +60,6 @@ var insertDocuments = function(db, callback) {
 	collection.insertMany([
 		{a : 1}, {a : 2}, {a : 3}
 	], function(err, result) {
-		// assert.equal(err, null);
-		// assert.equal(3, result.result.n);
-		// assert.equal(3, result.ops.length);
 		console.log("Inserted 3 documents into the document collection");
 		callback(result);
 	});
@@ -84,10 +68,6 @@ var insertDocuments = function(db, callback) {
 var findCollections = function(db, callback) {
 	// Get all the collections 
 	db.listCollections().toArray(function(err, collInfos) {
-	    // collInfos is an array of collection info objects that look like:
-	    // { name: 'test', options: {} }
-	    // assert.ok(items.length >= 1);
-
 		console.log("Found the following Collections");
 		console.log("Number of Collections: " + collInfos.length );
 		console.dir(collInfos);
@@ -100,8 +80,6 @@ var findDocuments = function(db, callback) {
 	var collection = db.collection('documents');
 	// Find some documents 
 	collection.find({}).toArray(function(err, docs) {
-		// assert.equal(err, null);
-		// assert.equal(2, docs.length);
 		console.log("Found the following records");
 		console.log("Number of Records: " + docs.length );
 		console.dir(docs);
